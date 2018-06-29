feade5d246d41c6efee114cddae8a30e4cc10fa8
pgecsenyi <pgecsenyi@protonmail.com>
2018-06-30 00:51:04 +0200

    Add fmt and lint commands to run scripts

--- run.bat
+++ run.bat
@@ -5,6 +5,24 @@ SET GOPATH=%CD%
 
 IF /i "%1" == "build" (
 	go build -o bin/main.exe src/main.go
+) ELSE IF /i "%1" == "fmt" (
+	gofmt -w src
+	gofmt -w src/application
+	gofmt -w src/bll
+	gofmt -w src/bll/common
+	gofmt -w src/bll/report
+	gofmt -w src/bll/testutil
+	gofmt -w src/dal
+	gofmt -w src/util
+) ELSE IF /i "%1" == "lint" (
+	golint src
+	golint src/application
+	golint src/bll
+	golint src/bll/common
+	golint src/bll/report
+	golint src/bll/testutil
+	golint src/dal
+	golint src/util
 ) ELSE IF /i "%1" == "test" (
 	goconvey .
 )
--- run.sh
+++ run.sh
@@ -4,6 +4,24 @@ export GOPATH=$(pwd)
 
 if [ "$1" = "build" ]; then
     go build -o bin/main src/main.go
+elif [ "$1" = "fmt" ]; then
+	gofmt -w src
+	gofmt -w src/application
+	gofmt -w src/bll
+	gofmt -w src/bll/common
+	gofmt -w src/bll/report
+	gofmt -w src/bll/testutil
+	gofmt -w src/dal
+	gofmt -w src/util
+elif [ "$1" = "lint" ]; then
+	golint src
+	golint src/application
+	golint src/bll
+	golint src/bll/common
+	golint src/bll/report
+	golint src/bll/testutil
+	golint src/dal
+	golint src/util
 elif [ "$1" = "test" ]; then
     goconvey .
 fi
